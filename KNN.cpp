1f772ba1084628a89c0066760a343c718840f32e
tusharlalawat <80534075+tusharlalawat@users.noreply.github.com>
2021-08-25 21:00:16 +0530

    Update KNN.cpp

--- KNN.cpp
+++ KNN.cpp
@@ -11,41 +11,137 @@ bool cmp(pair<int,int> p1, pair<int,int> p2)
     return (p1.first < p2.first);
 }
 
-int power(int n, int m)
+//To split data into train dataset and test dataset
+void split(vector<first> &train, vector<first> &test)
 {
-    if(m != 0)
-        return(n*power(n,m-1));
-    else
-        return 1;
-}
+    fstream f;
+    f.open("/home/cryptoduh/Downloads/Iris.csv",ios::in);
+    string line;
+    getline(f,line);
+    int d=1;
+    first tr,te;
 
-float stringTofloat(string s)
-{
-    int n = s.length();
-    if(n == 1)
+    int n = (70*50)/100;   //To make 70% data as train data of each flower type
+    
+    //To split Setosa flower type into train data and test data
+    while(d != 51 && getline(f,line))
     {
-        float r = int(s[0])-48;
-        return r;
+        stringstream s(line);
+        string temp;
+        if(d < n)
+        {
+            int i = 0;
+            while(getline(s, temp, ','))
+            {
+                if(i != 4)
+                {
+                    float result = std::stof(temp);
+                    tr.arr[i] = result;
+                    i++;
+                }
+            }
+            tr.type = 1;
+            train.push_back(tr);
+            d++;
+        }
+        else
+        {
+            int i = 0;
+            while(getline(s, temp, ','))
+            {
+                if(i != 4)
+                {
+                    float result = std::stof(temp);
+                    te.arr[i] = result;
+                    i++;
+                }
+            }
+            te.type = 1;
+            test.push_back(te);
+            d++;
+        }
     }
 
-    int d;
-    for(int i=0; i<n; i++)
+    //To split Versicolor flowers data into train data and test data
+    while(d != 101 && getline(f,line))
     {
-        if(s[i] == '.')
-            d = i;
+        stringstream s(line);
+        string temp;
+        if(d < 50+n)
+        {
+            int i = 0;
+            while(getline(s, temp, ','))
+            {
+                if(i != 4)
+                {
+                    float result = std::stof(temp);
+                    tr.arr[i] = result;
+                    i++;
+                }
+            }
+            tr.type = 2;
+            train.push_back(tr);
+            d++;
+        }
+        else
+        {
+            int i = 0;
+            while(getline(s, temp, ','))
+            {
+                if(i != 4)
+                {
+                    float result = std::stof(temp);
+                    te.arr[i] = result;
+                    i++;
+                }
+            }
+            te.type = 2;
+            test.push_back(te);
+            d++;
+        }
     }
-    float r1=0;
-    float r2=0;
-    int x = n-d-1;
-    for(int i=0; i<d; i++)
-        r1 = r1*10 + (int(s[i])-48);
-    for(int i=d+1; i<n; i++)
-        r2 = r2*10 + (int(s[i])-48);
 
-    r2 = r2/power(10,x);
-    return (r1+r2);
+    //To split virginica flower data into train data and test data
+    while(d != 151 && getline(f,line))
+    {
+        stringstream s(line);
+        string temp;
+        if(d < 100+n)
+        {
+            int i = 0;
+            while(getline(s, temp, ','))
+            {
+                if(i != 4)
+                {
+                    float result = std::stof(temp);
+                    tr.arr[i] = result;
+                    i++;
+                }
+            }
+            tr.type = 3;
+            train.push_back(tr);
+            d++;
+        }
+        else
+        {
+            int i = 0;
+            while(getline(s, temp, ','))
+            {
+                if(i != 4)
+                {
+                    float result = std::stof(temp);
+                    te.arr[i] = result;
+                    i++;
+                }
+            }
+            te.type = 3;
+            test.push_back(te);
+            d++;
+        }
+    }
 }
 
+//Main function used to predict
 int KNN(int K, vector<first>&test, vector<first>&train)
 {
     int len = test.size();
@@ -60,12 +156,12 @@ int KNN(int K, vector<first>&test, vector<first>&train)
         {
             float d = 0;
             for(int k=0; k<K; k++)
-                d += pow(train[j].arr[k]-test[i].arr[k],2);
+                d += pow((train[j].arr[k]-test[i].arr[k]),2);
 
             g.push_back(make_pair(sqrt(d),train[j].type));
         }
 
-        sort(g.begin(), g.end(), cmp);
+        sort(g.begin(), g.end(), cmp);        //Sort distance to choose nearest K neighbours
 
         int s=0,ver=0,vir=0;
         for(int j=0; j<K; j++)
@@ -113,131 +209,30 @@ int KNN(int K, vector<first>&test, vector<first>&train)
     return correct;
 }
 
+//To print dataset
+void print(vector<first> v)
+{
+    for(int i=0; i<v.size(); i++)
+    {
+        for(int j=0; j<4; j++)
+        {
+            cout <<v[i].arr[j] <<" ";
+        }
+        cout <<v[i].type;
+        cout <<endl;
+    }
+    cout <<endl;
+}
 
 int main()
 {
-    fstream f;
-    f.open("Iris.csv",ios::in); //path to file
-    vector<first>t1,t2,t3,t4,train,test;
-    string line;
-    getline(f,line);
-    int d=1;
-    first tr,te;
+    vector<first> train, test;
+    split(train, test);
 
-    int n = 35;
-    while(d != 51 && getline(f,line))
-    {
-        stringstream s(line);
-        string temp;
-        if(d < n)
-        {
-            int i = 0;
-            while(getline(s, temp, ','))
-            {
-                if(i != 4)
-                {
-                    float result = stringTofloat(temp);
-                    tr.arr[i] = result;
-                    i++;
-                }
-            }
-            tr.type = 1;
-            train.push_back(tr);
-            d++;
-        }
-        else
-        {
-            int i = 0;
-            while(getline(s, temp, ','))
-            {
-                if(i != 4)
-                {
-                    float result = stringTofloat(temp);
-                    te.arr[i] = result;
-                    i++;
-                }
-            }
-            te.type = 1;
-            test.push_back(te);
-            d++;
-        }
-    }
-
-    while(d != 101 && getline(f,line))
-    {
-        stringstream s(line);
-        string temp;
-        if(d < 50+n)
-        {
-            int i = 0;
-            while(getline(s, temp, ','))
-            {
-                if(i != 4)
-                {
-                    float result = stringTofloat(temp);
-                    tr.arr[i] = result;
-                    i++;
-                }
-            }
-            tr.type = 2;
-            train.push_back(tr);
-            d++;
-        }
-        else
-        {
-            int i = 0;
-            while(getline(s, temp, ','))
-            {
-                if(i != 4)
-                {
-                    float result = stringTofloat(temp);
-                    te.arr[i] = result;
-                    i++;
-                }
-            }
-            te.type = 2;
-            test.push_back(te);
-            d++;
-        }
-    }
-
-    while(d != 151 && getline(f,line))
-    {
-        stringstream s(line);
-        string temp;
-        if(d < 100+n)
-        {
-            int i = 0;
-            while(getline(s, temp, ','))
-            {
-                if(i != 4)
-                {
-                    float result = stringTofloat(temp);
-                    tr.arr[i] = result;
-                    i++;
-                }
-            }
-            tr.type = 3;
-            train.push_back(tr);
-            d++;
-        }
-        else
-        {
-            int i = 0;
-            while(getline(s, temp, ','))
-            {
-                if(i != 4)
-                {
-                    float result = stringTofloat(temp);
-                    te.arr[i] = result;
-                    i++;
-                }
-            }
-            te.type = 3;
-            test.push_back(te);
-            d++;
-        }
-    }
+    cout <<"\nTrain data is: " <<endl;
+    print(train);
+    cout <<"\nTest data is: " <<endl;
+    print(test);
 
     int max = 0;
     int K;
@@ -255,5 +250,6 @@ int main()
         cout <<"Accuracy of k = " <<k <<" is " <<per <<"%" <<endl;
     }
 
-    cout <<"Maximum accuracy is at k = " <<K <<" and the accuracy is = " <<max <<"%" <<endl;
+    cout <<"\nMaximum accuracy is at k = " <<K <<" and the accuracy is = " <<max <<"%" <<endl;
+    
 }
